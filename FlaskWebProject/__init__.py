4fe6e7c75d389d0f418ad862ecd28496e3eac082
Daniela Becerra Gonzalez <danybecerr@gmail.com>
2020-06-11 22:21:10 -0500

    Add personal screenshots

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,23 +2,19 @@
 The flask application package.
 """
 import logging
-from flask.logging import create_logger
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
-from logging.handlers import RotatingFileHandler
-import os
 import sys
 from .ascii_header import header
-from logging.config import dictConfig
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 # Add any logging levels and handlers with app.logger
-handler = logging.StreamHandler(stream=sys.stdout) #'ext://sys.stdout')
+handler = logging.StreamHandler(stream=sys.stdout)
 handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s"))
 app.logger.addHandler(handler)
 app.logger.setLevel(logging.INFO)
